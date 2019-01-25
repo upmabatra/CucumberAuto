ed9048b42af2728382884f4206bf06c558b098d5
batra <batra@DESKTOP-0G35J8S>
2019-01-25 17:43:06 -0500

    Second commit

--- CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
+++ CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
@@ -21,6 +21,7 @@ public class MyFirstStepDef {
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\batra\\Desktop\\Udemy\\cucumber\\drivers\\chromedriver.exe");
+		System.out.println("openeing URL");
 	}
 	
 	@When("^User enters user first name$")
